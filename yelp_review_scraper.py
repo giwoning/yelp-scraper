cd53c803dd274f523cbbe545d5d1e84b9c338d7c
Giwon Son <xpud0l@gmail.com>
2024-04-03 20:35:44 +0900

    NEVER GIVE UP

--- yelp_review_scraper.py
+++ yelp_review_scraper.py
@@ -647,8 +647,8 @@ def review_scraper(driver, index, res, list_of_page=[]):
             previous_sleep_time_within_page = random_sleep_within_page
             navigation_elements = driver.find_elements(By.XPATH, './/div[@aria-label="Pagination navigation"]')
             current_page_num = int(navigation_elements[0].find_elements(By.XPATH, './div[2]/span')[0].text.split('of')[0])
-            print(current_page_num)
-            print(total_page)
+            this_total_page_num = int(
+                navigation_elements[0].find_elements(By.XPATH, './div[2]/span')[0].text.split('of')[1])
             if current_page_num > total_page:
                 wrong = True
                 logger.info('This page number is larger than total page number. Continue to next page...')
